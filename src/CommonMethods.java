df353fbf57637c2edf350780fe9e376a19991c95
aka4alova <znastya@bk.ru>
2021-05-03 17:22:36 +0300

    Ex5: Тест: сохранение двух статей

---
+++ src/CommonMethods.java
@@ -0,0 +1,109 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.URL;
+import java.util.List;
+
+public class CommonMethods {
+
+    protected AppiumDriver<?> driver;
+
+    @Before
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "androidTestDevice");
+        capabilities.setCapability("platformVersion", "8.0");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "/Users/anastasiia.kachalova/Automation/Projects/JavaAppiumAutomation/apks/org.wiki.apk");
+        capabilities.setCapability(MobileCapabilityType.FULL_RESET, false);
+        capabilities.setCapability(MobileCapabilityType.NO_RESET, false);
+
+        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        Utils.sleep(4);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    protected WebElement waitForElementPresent(By by, String errorMessage, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(errorMessage);
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    protected WebElement waitForElementAndClick(By by, String errorMessage, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, errorMessage, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+
+    protected WebElement waitForElementAndClear(By by, String errorMessage, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, errorMessage, timeoutInSeconds);
+        element.clear();
+        return element;
+    }
+
+    protected WebElement waitForElementAndSendKeys(By by, String errorMessage, long timeoutInSeconds, String searchText) {
+        WebElement element = waitForElementPresent(by, errorMessage, timeoutInSeconds);
+        element.sendKeys(searchText);
+        return element;
+    }
+
+    protected boolean waitForElementNotPresent(By by, String errorMessage, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(errorMessage + "\n");
+        return wait.until(
+                ExpectedConditions.invisibilityOfElementLocated(by)
+        );
+    }
+
+    protected void swipeElementToLeft(By by, String errorMessage) {
+        WebElement element = waitForElementPresent(by, errorMessage, 10);
+        int left_x = element.getLocation().getX();
+        int right_x = left_x + element.getSize().getWidth();
+        int upper_y = element.getLocation().getY();
+        int lower_y = upper_y + element.getSize().getHeight();
+        int middle_y = (upper_y + lower_y) / 2;
+        TouchAction action = new TouchAction(driver);
+        action
+                .press(right_x, middle_y)
+                .waitAction(300)
+                .moveTo(left_x, middle_y)
+                .release()
+                .perform();
+    }
+
+    protected String waitForElementAndGetAttribute(By by, String attribute, String errorMessage, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, errorMessage, timeoutInSeconds);
+        return element.getAttribute(attribute);
+    }
+
+    private int getAmountOfElements(By by){
+        List<?> elements = driver.findElements(by);
+        System.out.println("elements.size= " + elements.size());
+        return elements.size();
+    }
+
+    protected void assertElementIsPresent (By by, String errorMessage){
+        int amountOfElements = getAmountOfElements(by);
+        System.out.println("amountOfElements = " + amountOfElements);
+        if (amountOfElements<=0){
+            String defaultMessage = "An element '" + by.toString() + "' supposed to be present ";
+            throw new AssertionError(defaultMessage + "; " + errorMessage);
+        }
+    }
+}
---
+++ src/Utils.java
@@ -0,0 +1,11 @@
+public class Utils {
+
+    public static void sleep(int sleepTimeInSeconds) {
+        try {
+            Thread.sleep(sleepTimeInSeconds * 1000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
